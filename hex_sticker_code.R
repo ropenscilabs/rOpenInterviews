325cdf1a559946ec7a3dc3cd721f039b2a502421
Kelly O'Briant <kellyobriant@gmail.com>
2018-05-30 18:59:28 -0400

    Hex sticker code

---
+++ hex_sticker_code.R
@@ -0,0 +1,6 @@
+library(hexSticker)
+
+imgurl = "~/Downloads/Microphones.png"
+sticker(imgurl, package="rOpenInterviews", p_size=5, s_x=1, s_y=.75, s_width=.6,
+        h_color = "darkslategray", h_fill = "darkseagreen",
+        filename="~/Downloads/rOpenInterviews.png")
